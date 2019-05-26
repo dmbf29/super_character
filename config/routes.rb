7cf1ac501a0a7241111f24d99d97ed6b87c77b77
Douglas Berkley <douglasmberkley@gmail.com>
2019-05-26 20:40:58 +0900

    added index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'characters#index'
   get 'search', to: 'characters#search', as: :characters_search
-  resources :characters, only: [:new, :create] do
+  resources :characters, only: [:index, :new, :create] do
     resources :character_powers, only: [:new, :create]
   end
 end
