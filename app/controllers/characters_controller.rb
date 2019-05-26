4792553296ccbbbb3281b5a17986548bcd9f9db0
Douglas Berkley <douglasmberkley@gmail.com>
2019-05-27 07:24:25 +0900

    removed searching by params in the index and search actions of #characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,22 +1,14 @@
 class CharactersController < ApplicationController
   def index
-    if params[:query].blank?
-      @characters = Character.order(name: :asc)
-    else
-      @characters = Character.where('name ILIKE ?', "%#{params[:query]}%").order(name: :asc)
-    end
-    @powers = Power.order(name: :asc)
     @character_power = CharacterPower.new
+    @powers = Power.order(name: :asc)
+    @characters = Character.order(name: :asc)
   end
 
   def search
     @character_power = CharacterPower.new
     @powers = Power.order(name: :asc)
-    if params[:query].blank?
-      @characters = Character.order(name: :asc)
-    else
-      @characters = Character.where('name ILIKE ?', "%#{params[:query]}%").order(name: :asc)
-    end
+    @characters = Character.order(name: :asc)
     respond_to do |format|
       format.html { redirect_to characters_path }
       format.js
